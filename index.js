4ad0c0bfb046b7ee07036e22b629f1b61ea27384
Abdullah881485 <abdullahalasad106882@gmail.com>
2025-11-12 12:18:18 +0600

    Details api called

--- index.js
+++ index.js
@@ -47,12 +47,7 @@ async function run() {
             res.send(result);
         });
 
-        app.get("/transactionDetails/:id", async (req, res) => {
-            const id = req.params.id
-            const query = { _id: new ObjectId(id) }
-            const result = await transactionsCollection.findOne(query)
-            res.send(result)
-        })
+        
         app.get("/myTransaction/:category", async (req, res) => {
             const email = req.query.email;
             const category = req.params.category
