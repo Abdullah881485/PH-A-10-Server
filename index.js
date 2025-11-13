053f2c9e355eebbb653196333ab3b8695093f6ed
Abdullah881485 <abdullahalasad106882@gmail.com>
2025-11-13 11:56:21 +0600

    key secured

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Initialize Firebase Admin SDK
 
-const serviceAccount = require("./finease-firebase-adminsdk.json");
+// index.js
+const decoded = Buffer.from(process.env.FIREBASE_SERVICE_KEY, "base64").toString("utf8");
+const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -117,7 +119,7 @@ async function run() {
         })
 
 
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // await client.close();
