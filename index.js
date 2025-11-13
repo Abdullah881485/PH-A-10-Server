7f2fb02365a27e770d3a24849a62d4eaac31e16a
Abdullah881485 <abdullahalasad106882@gmail.com>
2025-11-13 07:14:52 +0600

    admin sdk added

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const admin = require("firebase-admin");
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.awdvvgg.mongodb.net/?appName=Cluster0`;
 
+
+// Initialize Firebase Admin SDK
+
+const serviceAccount = require("./finease-firebase-adminsdk.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+
 // middleware
 app.use(cors())
 app.use(express.json())
+
+const verifyFirebaseToken = (req, res, next) => {
+    const authorization = req.headers.authorization
+    if (!authorization) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    const token = authorization.split(' ')[1]
+
+    if (!token) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+
+
+    try {
+        const decoded = admin.auth().verifyIdToken(token)
+        req.token_email = decoded.email;
+        next()
+    } catch {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+
+}
 
 
 const client = new MongoClient(uri, {
